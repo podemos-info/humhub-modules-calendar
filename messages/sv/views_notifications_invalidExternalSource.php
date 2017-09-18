0c3ff94b7b6a79dd5646fa57d9186db45e860306
Malka <malka@localhost.localdomain>
2017-09-18 10:04:51 +0200

    Todos los idiomas preparados para la notificación de calendario externo incorrecto

---
+++ messages/sv/views_notifications_invalidExternalSource.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+	'You have one wrong external calendar. Please review and <a style="color:blue" href="{url}">correct it</a>.' => 'Tienes un calendario externo erroneo. Porfavor revísalo y <a style="color:blue" href="{url}"">corrígelo</a>.', 
+);
